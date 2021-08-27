701aaee2ecfcc62ef4ea539a6c12a0cf6f828c2c
diegor220 <78614307+diegor220@users.noreply.github.com>
2021-08-27 09:46:07 -0600

    Update Receiver.py

--- Motobit carro/Receiver.py
+++ Motobit carro/Receiver.py
@@ -1,25 +1,43 @@
 from microbit import *
+from motobit import *
 import radio
 
+radio.on()
+
 while True:
-    radio.on()
-
-    motobit = MotoBit()
-
-    motobit.enable() # Enable motor driver
-    
-    l = motobit.left_motor() # Acquire motor object
-    r = motobit.right_motor(invert=True) # Enable invert polarity
-    
-    
-    if radio.receive("left"):
-        l.forward(25)
-        r.reverse(50)
+        if button_a.is_pressed():
+            Motor.enable()
+            motor_left.set_speed(100)
+            motor_right.set_speed(100)
+            sleep(1000)
+            motor_left.set_speed(100, Motor.REVERSE)
+            motor_right.set_speed(100)
+            sleep(200)
+            motor_left.set_speed(100)
+            motor_right.set_speed(100)
+            sleep(1000)
+            Motor.disable()
+            
         
-    if radio.receive("front"):
-        l.forward(50)
-        r.reverse(50)
-    
-    if radio.receive("right"):
-        l.forward(50)
-        r.reverse(25)
+
+        l = motor_left
+        r = motor_right
+
+        if radio.receive() == 'left':
+        
+            l.set_speed(25)
+            r.set_speed(50)
+            sleep(1000)
+            
+        
+        if radio.receive() == 'front':
+            
+            l.set_speed(50)
+            r.set_speed(50)
+            
+        
+        if radio.receive() == 'right':
+            l.set_speed(50)
+            r.set_speed(25)
+
+        
